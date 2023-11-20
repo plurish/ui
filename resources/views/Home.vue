4f003a4035f9f00399af1f9e7f376513f7b04a82
MonRaes <moniquefmoraes34@gmail.com>
2023-11-20 15:15:59 -0300

    Novas cores

--- resources/views/Home.vue
+++ resources/views/Home.vue
@@ -4,10 +4,40 @@
     <body>
     <header>
         <a href="#" class="logo" style="--clr:#52c7ce;">Plurish</a>
-
     </header>
+    
     <section id="home">Home</section>
-    <section id="about"></section>
+    <section id="about">Teste
+        <div class="slider-content">
+            <input type="radio" name="btn-radio" id="radio1">
+            <input type="radio" name="btn-radio" id="radio2">
+            <input type="radio" name="btn-radio" id="radio3">
+
+            <div class="slide-box primeiro">
+                <img class="img-desktop" src="Banner_1.jpg" alt="slide 1">
+                <img class="img-mobile" src="Banner_1.jpg" alt="slide 2">
+            </div>
+
+            <div class="slide-box">
+                <img class="img-desktop" src="Banner_2.jpg" alt="slide 1">
+                <img class="img-mobile" src="Banner_2.jpg" alt="slide 2">
+            </div>
+
+            <div class="slide-box">
+                <img class="img-desktop" src="Banner_3.jpg" alt="slide 1">
+                <img class="img-mobile" src="Banner_3.jpg" alt="slide 2">
+            </div>
+
+            <div class="nav-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+                <div class="auto-btn3"></div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
     <section id="services">Services</section>
     <section id="portifolio">Portifolio</section>
     <section id="contact">Contact</section>
@@ -24,4 +54,3 @@ export default defineComponent({
     components: { Head }
 });
 </script>
-
