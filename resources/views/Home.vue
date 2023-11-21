38b986ea40ff3eab63de78bbc68ebb7c8cfe45e8
Ghitado <thiaguinho.020405@gmail.com>
2023-11-20 22:14:12 -0300

    grid layout home

--- resources/views/Home.vue
+++ resources/views/Home.vue
@@ -1,56 +1,47 @@
 <template>
-    <Head title="Home" />
+  <Head title="Home"><link rel="stylesheet" href="/hometest.css" /></Head>
 
-    <body>
-    <header>
-        <a href="#" class="logo" style="--clr:#52c7ce;">Plurish</a>
-    </header>
-    
-    <section id="home">Home</section>
-    <section id="about">Teste
-        <div class="slider-content">
-            <input type="radio" name="btn-radio" id="radio1">
-            <input type="radio" name="btn-radio" id="radio2">
-            <input type="radio" name="btn-radio" id="radio3">
+  <body>
+  <header>
+      <a href="#" class="logo" style="--clr:#52c7ce;">Plurish</a>
+  </header>
+  
+  <section class="emcima">
+    <div class="emcima-item"><p>1</p></div>
+    <div class="emcima-item"><p>2</p></div>
+    <div class="emcima-item"><p>3</p></div>
+    <div class="emcima-item"><p>4</p></div>
+  </section>
 
-            <div class="slide-box primeiro">
-                <img class="img-desktop" src="Banner_1.jpg" alt="slide 1">
-                <img class="img-mobile" src="Banner_1.jpg" alt="slide 2">
-            </div>
+  <section class="embaixo">
+    <div class="embaixo-item">
+      <p>a</p>
+    </div>
 
-            <div class="slide-box">
-                <img class="img-desktop" src="Banner_2.jpg" alt="slide 1">
-                <img class="img-mobile" src="Banner_2.jpg" alt="slide 2">
-            </div>
+    <div class="embaixo-div">
+      <div class="embaixo-item">
+        <p>b</p>
+      </div>
 
-            <div class="slide-box">
-                <img class="img-desktop" src="Banner_3.jpg" alt="slide 1">
-                <img class="img-mobile" src="Banner_3.jpg" alt="slide 2">
-            </div>
+      <div class="embaixo-item">
+        <p>c</p>
+      </div>
 
-            <div class="nav-auto">
-                <div class="auto-btn1"></div>
-                <div class="auto-btn2"></div>
-                <div class="auto-btn3"></div>
-
-                </div>
-            </div>
-
-        </div>
-    </section>
-    <section id="services">Services</section>
-    <section id="portifolio">Portifolio</section>
-    <section id="contact">Contact</section>
+      <div class="embaixo-item">
+        <p>d</p>
+      </div>
+    </div>
+  </section>
 
 </body>
     
 </template>
 
-<script lang='ts'>;
-import { defineComponent } from 'vue';
-import { Head } from '@inertiajs/vue3';
+<script lang='ts'>
+  import { defineComponent } from 'vue';
+  import { Head } from '@inertiajs/vue3';
 
-export default defineComponent({
-    components: { Head }
-});
+  export default defineComponent({
+      components: { Head }
+  });
 </script>
